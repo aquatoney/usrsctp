c9bc4c3ca7bfaf68aa55e87d39cb9b4a9b26c514
Hao Li <aquatoney@gmail.com>
2020-02-06 17:30:18 +0800

    Run script with sepearted ports
    Signed-off-by: Hao Li <aquatoney@gmail.com>

--- programs/bench.py
+++ programs/bench.py
@@ -4,11 +4,15 @@ import subprocess
 
 
 model = sys.argv[1]
-proc_num = int(sys.argv[2])
+proc_num_range = sys.argv[2]
+proc_num_start = int(proc_num_range.split(':')[0])
+proc_num_end = int(proc_num_range.split(':')[1])
+proc_num = proc_num_end - proc_num_start + 1
+
 
 if model == '-s':
-        exe = './discard_server '
-        for p in range(0, proc_num):
+        exe = './discard_server_upcall '
+        for p in range(proc_num_start, proc_num_end):
                 args = str(10000+p) + ' ' + str(20000+p) + ' > /dev/null &'
                 proc = subprocess.Popen(exe+args, shell=True)
 
@@ -16,14 +20,14 @@ if model == '-s':
         while True:
                 c = input()
                 if c == 'y' or c == 'Y':
-                        os.system('killall discard_server')
+                        os.system('killall discard_server_upcall')
                         break
 
 if model == '-c':
         server = sys.argv[3]
-        exe = './client '
-        for p in range(0, proc_num):
-                args = f'{server} 9 0 ' + str(20000+p) + ' ' + str(10000+p) + ' > /dev/null 2>&1  &'
+        exe = './client_upcall '
+        for p in range(proc_num_start, proc_num_end):
+                args = f'{server} 9 {p} ' + str(20000+p) + ' ' + str(10000+p) + ' > /dev/null 2>&1  &'
                 proc = subprocess.Popen(exe+args, shell=True)
                 print(exe+args)
 
@@ -31,5 +35,5 @@ if model == '-c':
         while True:
                 c = input()
                 if c == 'y' or c == 'Y':
-                        os.system('killall client')
+                        os.system('killall client_upcall')
                         break
